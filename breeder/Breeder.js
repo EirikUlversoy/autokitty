f8c0b36fbdda0931cf6676d96697becbbf8541ee
Eirik Ulversoy <eulverso@gmail.com>
2018-03-30 18:54:45 +0200

    breeder module added, and works

---
+++ breeder/Breeder.js
@@ -0,0 +1,165 @@
+var GeneDecoder = require("genedecoder")();
+function Breeder(generations_breeding_upper_limit, upper_wallet_address, web3){
+	let self = {};
+	self.o = {};
+	self.web3 = web3;
+	self.upper_wallet_address = upper_wallet_address;
+	self.generations_breeding_upper_limit = generations_breeding_upper_limit;
+	//Ck_contract needs to be initialized before breeding
+	self.ck_contract = null;
+
+	self.findBreedingPairsTargeted = function(cats,targetedTraits){
+		var traitLists = {}
+
+		for(var trait in targetedTraits){
+			traitLists[targetedTraits[trait]] = [];
+		}
+
+		for(var cat in cats){
+			//GeneDecoder.readKitten(cats[cat],targetedTraits);
+			var newKitten = GeneDecoder.simpleFilter(cats[cat],targetedTraits);
+			if(!isEmptyObject(newKitten.chanceOfTrait)){
+				newCats.push(newKitten);
+
+			}
+		}
+		console.log("Found " + newCats.length + " filtered cats!");
+		cats = newCats;
+	}
+	self.separateByGeneration = function(cats){
+		var filteredCatList = [];
+
+		for (var kitten in cats){
+
+			if(!self.o[cats[kitten].generation]){
+				self.o[cats[kitten].generation] = [];
+			}
+			if(cats[kitten].generation <= self.generations_breeding_upper_limit){
+				self.o[cats[kitten].generation].push(cats[kitten]);
+				filteredCatList.push(cats[kitten]);
+			}
+
+		}
+		return filteredCatList;
+	}
+
+	self.advancedBreedingLoop = function(){
+		var targetedTraits = [];
+		//Same as breeding loop but with trait selection
+	}
+	self.breedingLoop = function(cats, ck_contract){
+		self.ck_contract = ck_contract;
+		var filteredCatList = self.separateByGeneration(cats);
+		breedingPairs = self.findBreedingPairs(filteredCatList);
+		console.log('Kitten breeding pairs found: %d', breedingPairs.length);
+		console.log("Account used to breed: " + self.web3.eth.defaultAccount);
+	}
+	self.o = {};
+	self.readyToBreedCheckA = function(id, id2){
+		self.ck_contract.methods.isReadyToBreed(id).call().then(z => self.readyToBreedCheckB(id, id2, z));
+	}
+
+	self.readyToBreedCheckB = function(id, id2, aIsReady){
+		if(aIsReady){
+			self.ck_contract.methods.isReadyToBreed(id2).call().then(z => self.notOnAuctionCheckA(id, id2, z));
+		} else {
+			console.log("A not ready");
+		}
+	}
+
+	self.notOnAuctionCheckA = function(id, id2, bIsReady){
+		if(bIsReady){
+			self.ck_contract.methods.ownerOf(id).call().then(z => self.notOnAuctionCheckB(id, id2, z));
+		} else {
+			console.log("B not ready");
+		}
+	}
+	self.notOnAuctionCheckB = function(id, id2, address){
+		if(address != self.upper_wallet_address){
+			console.log("For sale, cannot breed");
+		} else {
+			self.ck_contract.methods.ownerOf(id2).call().then(z => self.canBreedWithCheck(id,id2,z));
+		}
+	}
+	self.canBreedWithCheck = function(id, id2, address){
+		if(address != upper_wallet_address){
+			console.log("for sale, cannot breed");
+		} else {
+			self.ck_contract.methods.canBreedWith(id,id2).call().then(z => self.triggerTransactionOnly(id,id2,z));		
+		}
+	}
+
+	self.triggerTransactionOnly = function(id, id2, canBreed){
+		if(canBreed){
+			self.ck_contract.methods.breedWithAuto(id, id2).send({from: self.web3.eth.defaultAccount, value: self.web3.utils.toWei("0.008", "ether"),gasPrice: self.web3.utils.toWei("0.000000007", "ether") });
+			console.log("Breeding: " + id +" and " + id2 + " together!");
+			//console.log("(((would have)))");
+		} else {
+			console.log("Breeding failed. Possible cause: Too close relation");
+		}
+	}
+	self.bothReady = function(cat, potentialPartner){
+		return cat.isReady && potentialPartner.isReady;
+	}
+
+	//Tuple describing a breeding pair
+	function BreedingPair(id1, id2){
+		this.id1 = id1;
+		this.id2 = id2;
+	}
+
+	//function to remove element from array
+	function remove(array, element){
+		return array.filter(function(e) { 
+	    return e !== element;
+	});
+	}
+	self.findBreedingPairs = function(cats){
+		var listOfUsedCats = [];
+		var newCats = [];
+		var breedingPairs = [];
+		console.log("Looking at " + cats.length + " total cats!");
+
+		for (var cat in cats){
+			count = cat;
+			cat = cats[cat];
+			var potentialPartners = self.o[cat.generation];
+			var potentialPartners = remove(potentialPartners, cat.id);
+
+			var tries = 0;
+			var maxTries = 100;
+			matchOrTimeOut = false;
+			if(cat){
+				while (!matchOrTimeOut){
+
+					var potentialPartner = potentialPartners[Math.floor(Math.random()*potentialPartners.length)];
+					if (self.ck_contract.methods.canBreedWith(cat.id,potentialPartner.id) && self.bothReady(cat, potentialPartner) && (cat.id != potentialPartner.id)){
+						if(!listOfUsedCats.includes(cat.id) && !listOfUsedCats.includes(potentialPartner.id)){
+							breedingPairs.push(new BreedingPair(cat.id,potentialPartner.id));
+							listOfUsedCats.push(cat.id);
+							listOfUsedCats.push(potentialPartner.id);
+							matchOrTimeOut = true;
+							setTimeout(self.readyToBreedCheckA,150*count, cat.id, potentialPartner.id);
+							self.o[cat.generation] = remove(self.o[cat.generation], potentialPartner.id);
+							self.o[cat.generation] = remove(self.o[cat.generation], cat.id);
+						}
+						
+					}
+					tries++;
+					if (tries > maxTries ){
+						matchOrTimeOut = true;
+
+					}
+
+				}	
+			}
+
+
+
+		}
+		return breedingPairs;
+	}
+	return self;
+}
+
+module.exports = Breeder;
--- cktest.js
+++ cktest.js
@@ -1,11 +1,17 @@
 var CKClient = require("ckclient")();
 var net = require('net');
+var upper_wallet_address = "0x68b42e44079D1d0A4a037e8c6eCd62c48967e69f";
 var Web3 = require("Web3");
 var fs = require("fs");
 var Promise = require("bluebird");
 var AdvancedBreeder = require('./advKittenBreedingFunctions');
 var GeneDecoder = require("genedecoder")();
+var generations_breeding_upper_limit = 1;
 var web3 = new Web3(new Web3.providers.IpcProvider('\\\\.\\pipe\\geth.ipc', net));
+
+var Breeder = require("breeder")(generations_breeding_upper_limit,upper_wallet_address, web3);
+console.log(Breeder);
+//Breeder = Breeder(generations_breeding_upper_limit);
 var promiseLimit = require('promise-limit')
 
 var api_calls_on = false;
@@ -15,7 +21,6 @@ var owner_wallet_address = "0x68b42e44079d1d0a4a037e8c6ecd62c48967e69f";
 
 //Contract address of cryptokitties (does not change)
 var cryptokitties_contract_address = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d";
-var upper_wallet_address = "0x68b42e44079D1d0A4a037e8c6eCd62c48967e69f";
 //Kitty Contract ABI, does not change
 var kitty_abi = 
 [{"constant":true,"inputs":[{"name":"_interfaceID","type":"bytes4"}],"name":"supportsInterface","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"cfoAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"},{"name":"_preferredTransport","type":"string"}],"name":"tokenMetadata","outputs":[{"name":"infoUrl","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"promoCreatedCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"ceoAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"GEN0_STARTING_PRICE","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"setSiringAuctionAddress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"pregnantKitties","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_kittyId","type":"uint256"}],"name":"isPregnant","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"GEN0_AUCTION_DURATION","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"siringAuction","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"setGeneScienceAddress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_newCEO","type":"address"}],"name":"setCEO","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_newCOO","type":"address"}],"name":"setCOO","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_kittyId","type":"uint256"},{"name":"_startingPrice","type":"uint256"},{"name":"_endingPrice","type":"uint256"},{"name":"_duration","type":"uint256"}],"name":"createSaleAuction","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"sireAllowedToAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_matronId","type":"uint256"},{"name":"_sireId","type":"uint256"}],"name":"canBreedWith","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"kittyIndexToApproved","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_kittyId","type":"uint256"},{"name":"_startingPrice","type":"uint256"},{"name":"_endingPrice","type":"uint256"},{"name":"_duration","type":"uint256"}],"name":"createSiringAuction","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"val","type":"uint256"}],"name":"setAutoBirthFee","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_addr","type":"address"},{"name":"_sireId","type":"uint256"}],"name":"approveSiring","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_newCFO","type":"address"}],"name":"setCFO","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_genes","type":"uint256"},{"name":"_owner","type":"address"}],"name":"createPromoKitty","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"secs","type":"uint256"}],"name":"setSecondsPerBlock","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"withdrawBalance","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"name":"owner","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"GEN0_CREATION_LIMIT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"newContractAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"setSaleAuctionAddress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_v2Address","type":"address"}],"name":"setNewAddress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"secondsPerBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"tokensOfOwner","outputs":[{"name":"ownerTokens","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_matronId","type":"uint256"}],"name":"giveBirth","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"withdrawAuctionBalances","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"cooldowns","outputs":[{"name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"kittyIndexToOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"cooAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"autoBirthFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"erc721Metadata","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_genes","type":"uint256"}],"name":"createGen0Auction","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_kittyId","type":"uint256"}],"name":"isReadyToBreed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PROMO_CREATION_LIMIT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_contractAddress","type":"address"}],"name":"setMetadataAddress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"saleAuction","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_id","type":"uint256"}],"name":"getKitty","outputs":[{"name":"isGestating","type":"bool"},{"name":"isReady","type":"bool"},{"name":"cooldownIndex","type":"uint256"},{"name":"nextActionAt","type":"uint256"},{"name":"siringWithId","type":"uint256"},{"name":"birthTime","type":"uint256"},{"name":"matronId","type":"uint256"},{"name":"sireId","type":"uint256"},{"name":"generation","type":"uint256"},{"name":"genes","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_sireId","type":"uint256"},{"name":"_matronId","type":"uint256"}],"name":"bidOnSiringAuction","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"gen0CreatedCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"geneScience","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_matronId","type":"uint256"},{"name":"_sireId","type":"uint256"}],"name":"breedWithAuto","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"owner","type":"address"},{"indexed":false,"name":"matronId","type":"uint256"},{"indexed":false,"name":"sireId","type":"uint256"},{"indexed":false,"name":"cooldownEndBlock","type":"uint256"}],"name":"Pregnant","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"from","type":"address"},{"indexed":false,"name":"to","type":"address"},{"indexed":false,"name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"owner","type":"address"},{"indexed":false,"name":"approved","type":"address"},{"indexed":false,"name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"owner","type":"address"},{"indexed":false,"name":"kittyId","type":"uint256"},{"indexed":false,"name":"matronId","type":"uint256"},{"indexed":false,"name":"sireId","type":"uint256"},{"indexed":false,"name":"genes","type":"uint256"}],"name":"Birth","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newContract","type":"address"}],"name":"ContractUpgrade","type":"event"}];
@@ -56,11 +61,7 @@ function Cat(id, generation, attributeList) {
 	this.attributeList = attributeList;
 }
 
-//Tuple describing a breeding pair
-function BreedingPair(id1, id2){
-	this.id1 = id1;
-	this.id2 = id2;
-}
+
 
 var callsMade = 0;
 var callsHandled = 0;
@@ -142,44 +143,8 @@ function saveKittenIds(kittens){
   	console.log('It\'s saved!');
 });}
 
-
-function refreshCats(){
-
-}
-function breedingLoop(){
-	var filteredCatList = [];
-	//cats = [];
-	//var kittens = loopGetUserKittesNAPI();
-	//handleKittensWithID(kittens);
-	o = {};
-	for (var kitten in cats){
-
-		if(!o[cats[kitten].generation]){
-			o[cats[kitten].generation] = [];
-		}
-		if(cats[kitten].generation <= generations_breeding_upper_limit){
-			o[cats[kitten].generation].push(cats[kitten]);
-			filteredCatList.push(cats[kitten]);
-		}
-
-	}
-
-	//var catsToBeAuctioned = findAuctionItems(cats);
-	var testTargetedTraits = ["Cyan"];
-	var targetedTraits = [];
-	targetedTraits = testTargetedTraits;
-	findBreedingPairs(filteredCatList,targetedTraits);
-	console.log('Kitten breeding pairs found: %d', breedingPairs.length);
-	//console.log("Auctionable items found: %d", catsToBeAuctioned.length);
-	//console.log(breedingPairs);
-	console.log("Using account: " + web3.eth.defaultAccount);
-	console.log("done");
-}
-var generations_breeding_upper_limit = 6;
 function mainFunction (calls){
 	console.log("is in main");
-	//console.log(calls);
-	//cats = calls;
 	if(api_calls_on){
 		saveKittenIds(cats);
 	}
@@ -187,7 +152,7 @@ function mainFunction (calls){
 
 	//findAuctionItems(cats);
 
-	breedingLoop();
+	Breeder.breedingLoop(cats, ck_contract);
 
 }
 
@@ -308,11 +273,11 @@ function chunkify(a, n, balanced) {
 }
 var allFilteredCatIDs = [];
 function helper(){
-	//var kittens = loopGetUserKittesNAPI();
+	var kittens = loopGetUserKittesNAPI();
 	
 	//var kittens = grabAllOwnedCatsFromBlockchain();
-	var kittens = allFilteredCatIDs;
-	console.log(allFilteredCatIDs.length + " cats owned by own address");
+	//var kittens = allFilteredCatIDs;
+	//console.log(allFilteredCatIDs.length + " cats owned by own address");
 	var kittenArrays = chunkify(kittens,1,false);
 	var promiseArrayStack = [];
 	for(var kittenArray in kittenArrays){
@@ -326,61 +291,14 @@ function helper(){
 if(api_calls_on){
 	loopGetUserKitties().then(mainFunction);
 } else {
-	helper().then(mainFunction);
+	Promise.delay(10000).then(helper().then(mainFunction));
 
 	//loopGetUserKittesNAPI().then(handleKittensWithID).then(mainFunction);
 }
 
-//function to remove element from array
-function remove(array, element){
-	return array.filter(function(e) { 
-    return e !== element;
-});
-}
 
-function readyToBreedCheckA(id, id2){
-	ck_contract.methods.isReadyToBreed(id).call().then(z => readyToBreedCheckB(id, id2, z));
-}
 
-function readyToBreedCheckB(id, id2, aIsReady){
-	if(aIsReady){
-		ck_contract.methods.isReadyToBreed(id2).call().then(z => notOnAuctionCheckA(id, id2, z));
-	} else {
-		console.log("A not ready");
-	}
-}
 
-function notOnAuctionCheckA(id, id2, bIsReady){
-	if(bIsReady){
-		ck_contract.methods.ownerOf(id).call().then(z => notOnAuctionCheckB(id, id2, z));
-	} else {
-		console.log("B not ready");
-	}
-}
-function notOnAuctionCheckB(id, id2, address){
-	if(address != upper_wallet_address){
-		console.log("For sale, cannot breed");
-	} else {
-		ck_contract.methods.ownerOf(id2).call().then(z => canBreedWithCheck(id,id2,z));
-	}
-}
-function canBreedWithCheck(id, id2, address){
-	if(address != upper_wallet_address){
-		console.log("for sale, cannot breed");
-	} else {
-		ck_contract.methods.canBreedWith(id,id2).call().then(z => triggerTransactionOnly(id,id2,z));		
-	}
-}
-
-function triggerTransactionOnly(id, id2, canBreed){
-	if(canBreed){
-		//ck_contract.methods.breedWithAuto(id, id2).send({from: web3.eth.defaultAccount, value: web3.utils.toWei("0.008", "ether"),gasPrice: web3.utils.toWei("0.000000007", "ether") });
-		console.log("Breeding: " + id +" and " + id2 + " together!");
-		console.log("(((would have)))");
-	} else {
-		console.log("Breed with each other fail");
-	}
-}
 
 function check(id){
 	ck_contract.methods.isPregnant(id).call().then(z => secondCheck(id,z));
@@ -433,74 +351,10 @@ function findAuctionItems(cats_current){
 function pairHitsAll(cat_one,cat_two){
 	//pseudo: if cat pair has all needed traits in combination => breed
 }
-function findBreedingPairsTargeted(cats,targetedTraits){
-	var traitLists = {}
 
-	for(var trait in targetedTraits){
-		traitLists[targetedTraits[trait]] = [];
-	}
-
-	for(var cat in cats){
-		//GeneDecoder.readKitten(cats[cat],targetedTraits);
-		var newKitten = GeneDecoder.simpleFilter(cats[cat],targetedTraits);
-		if(!isEmptyObject(newKitten.chanceOfTrait)){
-			newCats.push(newKitten);
-
-		}
-	}
-	console.log("Found " + newCats.length + " filtered cats!");
-	cats = newCats;
-}
 
 function findBreedingPairs(cats, targetedTraits){
-	var listOfUsedCats = [];
-	var newCats = [];
-	console.log("Looking at " + cats.length + " total cats!");
-	
-	
-	for (var cat in cats){
-		count = cat;
-		cat = cats[cat];
-		var potentialPartners = o[cat.generation];
-		var potentialPartners = remove(potentialPartners, cat.id);
 
-		var tries = 0;
-		var maxTries = 100;
-		matchOrTimeOut = false;
-		if(cat){
-			while (!matchOrTimeOut){
-
-				var potentialPartner = potentialPartners[Math.floor(Math.random()*potentialPartners.length)];
-				//bothReady = ck_contract.methods.isReadyToBreed(cat.id) && ck_contract.methods.isReadyToBreed(potentialPartner.id);
-				bothReady = cat.isReady && potentialPartner.isReady
-				//bothNotPregnant = !ck_contract.methods.isPregnant(cat.id) && !ck_contract.methods.isPregnant(potentialPartner.id);
-				if (ck_contract.methods.canBreedWith(cat.id,potentialPartner.id) && bothReady && (cat.id != potentialPartner.id)){
-
-
-					if(!listOfUsedCats.includes(cat.id) && !listOfUsedCats.includes(potentialPartner.id)){
-						breedingPairs.push(new BreedingPair(cat.id,potentialPartner.id));
-						listOfUsedCats.push(cat.id);
-						listOfUsedCats.push(potentialPartner.id);
-						matchOrTimeOut = true;
-						setTimeout(readyToBreedCheckA,150*count, cat.id, potentialPartner.id);
-						//readyToBreedCheckA(cat.id,potentialPartner.id);
-						o[cat.generation] = remove(o[cat.generation], potentialPartner.id);
-						o[cat.generation] = remove(o[cat.generation], cat.id);
-					}
-					
-				}
-				tries++;
-				if (tries > maxTries ){
-					matchOrTimeOut = true;
-
-				}
-
-			}	
-		}
-
-
-
-	}
 
 }
 
